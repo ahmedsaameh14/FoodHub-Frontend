85afbd41763a7b9afdf3c0e283441fda8b0bd364
Ahmed Sameh <ahmedsaameh14@gmail.com>
2025-12-23 00:36:04 +0200

    Complete Registration

--- src/app/Sign/registration/registration.component.ts
+++ src/app/Sign/registration/registration.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { SignService } from '../../Core/Services/sign.service';
+import { Router, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ReactiveFormsModule , CommonModule , RouterLink],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  constructor(private _sign:SignService , private _router:Router){}
 
+  regForm: FormGroup = new FormGroup({
+    name: new FormControl('' , [Validators.required]),
+    email: new FormControl('' , [Validators.required , Validators.email]),
+    password: new FormControl('' , [Validators.required , Validators.minLength(6)])
+  });
+
+  onSubmit(){
+    if(this.regForm.invalid){
+      this.regForm.markAllAsTouched();
+      return;
+    }
+
+    this._sign.registration(this.regForm.value).subscribe({
+      next: (res) =>{
+        console.log('Registration Successful' , res);
+        this._router.navigate(['/login'])
+      },
+      error: (err) => console.log('Registration Failed' , err),
+    })
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegistrationComponent } from './Sign/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'registration' , component: RegistrationComponent},
+
+];
