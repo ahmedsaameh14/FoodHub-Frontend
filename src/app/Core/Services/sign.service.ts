6e9ac49f200bf293401b508cee9515673d9f017c
Ahmed Sameh <ahmedsaameh14@gmail.com>
2025-12-22 21:23:21 +0200

    Reg Service and Model(interface)

--- src/app/Core/Services/sign.service.ts
+++ src/app/Core/Services/sign.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environment/environment';
+import { Observable } from 'rxjs';
+import { IReg } from '../Models/sign';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignService {
 
-  constructor() { }
+  constructor( private _http:HttpClient ) { }
+
+  URL = environment.API_URL;
+
+  registration(userdata:IReg): Observable<IReg>{
+    return this._http.post<IReg>(`${this.URL}/registration` , userdata)
+  }
+
+  
 }
