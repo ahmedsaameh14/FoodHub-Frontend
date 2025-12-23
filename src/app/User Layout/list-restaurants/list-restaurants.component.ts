8b9063e6f482b9ee65d081a35f7bccb8bf509083
Ahmed Sameh <ahmedsaameh14@gmail.com>
2025-12-23 17:40:12 +0200

    Create Main User Layout Components

---
+++ src/app/User Layout/list-restaurants/list-restaurants.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-restaurants',
+  imports: [],
+  templateUrl: './list-restaurants.component.html',
+  styleUrl: './list-restaurants.component.css'
+})
+export class ListRestaurantsComponent {
+
+}
