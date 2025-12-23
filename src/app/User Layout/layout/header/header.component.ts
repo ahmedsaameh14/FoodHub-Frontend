c724a189c506d1b7809a4a08f236abdfa2a2908e
Ahmed Sameh <ahmedsaameh14@gmail.com>
2025-12-24 00:04:16 +0200

    Complete Header User Layout

--- src/app/User Layout/layout/header/header.component.ts
+++ src/app/User Layout/layout/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IUser } from '../../../Core/Models/sign';
+import { SignService } from '../../../Core/Services/sign.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  user$: Observable<IUser | any>;
+  constructor(private _sign: SignService, private _router: Router) {
+    this.user$ = this._sign.user$;
+  }
 
+  logout() {
+    this._sign.logout();
+    this._router.navigate(['/home']);
+  }
 }
--- src/app/User Layout/layout/layout.component.ts
+++ src/app/User Layout/layout/layout.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet , HeaderComponent , FooterComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
 
