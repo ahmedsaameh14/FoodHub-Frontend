33c61c1ff3859b9bdf9d38b0d2821e7c54a99807
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-02-24 21:09:11 +0200

    Complete User Home

--- src/app/User Layout/home/home.component.ts
+++ src/app/User Layout/home/home.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { IRestaurant } from '../../Core/Models/restaurant';
+import { RestaurantService } from '../../Core/Services/restaurant.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  restaurants: IRestaurant[] = [];
+  contactData = { name: '', email: '', phone: '', message: '' };
 
+  constructor(private _restS: RestaurantService) {}
+
+  ngOnInit() {
+    this.getFeaturedRestaurants();
+  }
+
+  getFeaturedRestaurants() {
+    this._restS.getRestaurants(1, 10).subscribe((res) => {
+      this.restaurants = res.result;
+    });
+  }
+
+  scrollToRestaurants() {
+    document.getElementById('restaurants-section')?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  onSubmitContact() {
+    console.log('Form Data:', this.contactData);
+    alert('Message sent successfully!');
+  }
 }
