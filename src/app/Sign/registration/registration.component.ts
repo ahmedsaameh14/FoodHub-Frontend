3aa5e5fe879720625c39b77c4fcfa61bf86df14b
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-02-25 15:54:06 +0200

    Edit on UI of Sign

--- src/app/Sign/registration/registration.component.ts
+++ src/app/Sign/registration/registration.component.ts
@@ -1,36 +1,37 @@
 import { Component } from '@angular/core';
-import { SignService } from '../../Core/Services/sign.service';
-import { Router, RouterLink } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { SignService } from '../../Core/Services/sign.service';
 
 @Component({
   selector: 'app-registration',
-  imports: [ReactiveFormsModule , CommonModule , RouterLink],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-  constructor(private _sign:SignService , private _router:Router){}
-
   regForm: FormGroup = new FormGroup({
-    name: new FormControl('' , [Validators.required]),
-    email: new FormControl('' , [Validators.required , Validators.email]),
-    password: new FormControl('' , [Validators.required , Validators.minLength(6)])
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
 
-  onSubmit(){
-    if(this.regForm.invalid){
+  constructor(private _sign: SignService, private _router: Router) {}
+
+  onSubmit() {
+    if (this.regForm.invalid) {
       this.regForm.markAllAsTouched();
       return;
     }
 
     this._sign.registration(this.regForm.value).subscribe({
-      next: (res) =>{
-        console.log('Registration Successful' , res);
-        this._router.navigate(['/login'])
+      next: (res) => {
+        console.log('Success:', res);
+        this._router.navigate(['/login']);
       },
-      error: (err) => console.log('Registration Failed' , err),
-    })
+      error: (err) => console.error('Registration error:', err)
+    });
   }
 }
